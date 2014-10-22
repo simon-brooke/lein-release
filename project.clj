7c17376f2487843117846122fbdba78c5297416e
Kyle Burton <kyle.burton@gmail.com>
2014-10-22 14:02:42 -0400

    trying to deploy

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Leiningen Release Plugin"
   :url         "https://github.com/relaynetwork/lein-release"
   :dev-dependencies [[swank-clojure "1.4.2"]]
+  :repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
   :eval-in :leiningen
   :lein-release {:deploy-via :clojars}
   :dependencies [[org.clojure/clojure "1.3.0"]])
