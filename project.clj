7dbb9404d7d56e3c4229c5b2306ace74a175ef19
Kyle Burton <kyle.burton@gmail.com>
2015-05-14 12:06:18 -0400

    bump for next dev cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-release/lein-release "1.0.7"
+(defproject lein-release/lein-release "1.0.8-SNAPSHOT"
   :description "Leiningen Release Plugin"
   :url         "https://github.com/relaynetwork/lein-release"
   :dev-dependencies [[swank-clojure "1.4.2"]]
